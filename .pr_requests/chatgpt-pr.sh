6084ed50627dd5ed63a2010ddf127cf5d58dff25
Fahd Ayoubi <fayoubi@gmail.com>
2025-08-21 12:06:04 +0100

    testing

---
+++ .pr_requests/chatgpt-pr.sh
@@ -0,0 +1,5 @@
+echo "We are creating a webservice and a simple UI that will live in the repo above." > .pr_requests/test-readme.txt
+git add .pr_requests/test-readme.txt
+git commit -m "Test PR request"
+git push origin main
+
